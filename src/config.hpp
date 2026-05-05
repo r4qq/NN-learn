18fbba3f796091dbd6872436189dd14f5ab935aa
r4qq <r4qq@protonmail.com>
2026-05-06 01:04:52 +0200

    add comments in config.hpp so clangd doesnt shoot with warnings

--- src/config.hpp
+++ src/config.hpp
@@ -7,11 +7,11 @@
 #define TENSOR_TYPE 2
 
 #if TENSOR_TYPE == 2
-#   include "tensor-hpp/Tensor-simd-block.hpp"
+#   include "tensor-hpp/Tensor-simd-block.hpp" // IWYU pragma: export
 #elif TENSOR_TYPE == 1
-#   include "tensor-hpp/Tensor-simd.hpp"
+#   include "tensor-hpp/Tensor-simd.hpp" // IWYU pragma: export
 #elif TENSOR_TYPE == 0
-#   include "tensor-hpp/Tensor.hpp"
+#   include "tensor-hpp/Tensor.hpp" // IWYU pragma: export
 #else
 #   error invalid tensor type
 #endif
