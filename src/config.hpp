e82ec0515fbec26f59b8574cd74545863ac3ed5b
r4qq <r4qq@protonmail.com>
2026-06-21 22:31:47 +0200

    update config hpp

--- src/config.hpp
+++ src/config.hpp
@@ -3,10 +3,13 @@
 #define TYPE_REGULAR 0
 #define TYPE_SIMD 1
 #define TYPE_SIMD_BLOCK 2
+#define TYPE_SIMD_BLOCK_MULTI 3
 
-#define TENSOR_TYPE 2
+#define TENSOR_TYPE 3
 
-#if TENSOR_TYPE == 2
+#if TENSOR_TYPE == 3
+#   include "tensor-hpp/Tensor-simd-block-multi.hpp" // IWYU pragma: export
+#elif TENSOR_TYPE == 2
 #   include "tensor-hpp/Tensor-simd-block.hpp" // IWYU pragma: export
 #elif TENSOR_TYPE == 1
 #   include "tensor-hpp/Tensor-simd.hpp" // IWYU pragma: export
