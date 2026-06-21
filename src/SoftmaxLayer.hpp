8ff069a0ebdb3d56861bcc80e4cfff9d5fd489e1
r4qq <r4qq@protonmail.com>
2026-06-21 20:55:12 +0200

    finish softmax layer

--- src/SoftmaxLayer.hpp
+++ src/SoftmaxLayer.hpp
@@ -1,8 +1,8 @@
 #include "Layer.hpp"
+#include "tensor-hpp/Tensor-simd-block.hpp"
 #include <algorithm>
 #include <cmath>
 #include <cstdint>
-#include <functional>
 #include <numeric>
 
 template<typename T>
@@ -10,32 +10,143 @@ class SoftmaxLayer : public Layer<T>
 {
     private:
         Tensor::Tensor<T> _cachedOutput;
+        Tensor::Tensor<T> _cachedInputGrad;
 
     public:
-        SoftmaxLayer() : _cachedOutput({1}) {}
+        SoftmaxLayer() :
+            _cachedOutput({1}),
+            _cachedInputGrad({1}){} 
 
         Tensor::Tensor<T> forward(const Tensor::Tensor<T>& input) override
         {
-            //Tensor::Tensor<T> outputTensor(input.shape());
+            if (_cachedOutput.shape() != input.shape()) [[unlikely]]
+            {
+                _cachedOutput = Tensor::Tensor<T>(input.shape()); 
+            }
 
             uint64_t batchSize = input.shape()[0];
             uint64_t numClasses = input.shape()[1];
 
             for (uint64_t i = 0; i < batchSize; ++i) 
             {
-                auto M = std::max_element(input.data(), input.data() + (i * numClasses));
-                T localSum = std::transform_reduce(input.data(), 
-                                                input.data() + input.size(), 
-                                                T{0}, 
-                                                std::plus<T>(), 
-                                                [M](T val){
-                                                    std::exp(val - M);
-                                                });                
+                auto rowStartIn = input.data() + (i * numClasses);
+                auto rowEndIn = rowStartIn + numClasses;
+            
+                auto rowStartOut = _cachedOutput.data() + (i * numClasses);
+                auto rowEndOut = rowStartOut + numClasses;
+            
+                auto maxIter = std::max_element(rowStartIn, rowEndIn);
+                T rowMax = *maxIter;
+
+                std::transform(rowStartIn, 
+                                rowEndIn, 
+                                rowStartOut, 
+                                [rowMax](T val){
+                                    return std::exp(val - rowMax);
+                                });
+                
+                auto localSum = std::accumulate(rowStartOut, rowEndOut, T{0});
+
+                std::transform(rowStartOut,
+                               rowEndOut,
+                               rowStartOut,
+                               [localSum](T val){
+                                   return val / localSum;
+                               });                
             }
 
-            // for (auto val : input.data()) {
-            //     val
-            // }
             return _cachedOutput;
+        }
+
+        Tensor::Tensor<T> backward(const Tensor::Tensor<T>& outputGradient, T learningRate) override 
+        {
+            if (_cachedInputGrad.shape() != outputGradient.shape()) [[unlikely]]
+            {
+                _cachedInputGrad = Tensor::Tensor<T>(outputGradient.shape()); 
+            }
+
+            uint64_t batchSize = outputGradient.shape()[0];
+            uint64_t numClasses = outputGradient.shape()[1];
+
+            for (uint64_t i = 0; i < batchSize; ++i) 
+            {
+                auto sStart = _cachedOutput.data() + (i * numClasses);
+                auto sEnd = sStart + numClasses;
+
+                auto gStart = outputGradient.data() + (i * numClasses);
+                auto dZStart = _cachedInputGrad.data() + (i * numClasses);
+
+                auto dotRowProduct = std::inner_product(sStart, 
+                                                        sEnd, 
+                                                        gStart, 
+                                                        T{0});
+
+                std::transform(sStart, 
+                               sEnd, 
+                               gStart, 
+                               dZStart,
+                               [dotRowProduct](T sVal, T gVal){
+                                    return sVal * (gVal - dotRowProduct);
+                               });
+            }
+
+            return _cachedInputGrad;
+        }
+};
+
+template<typename T>
+class CCELSoftmaxLayer : public Layer<T>
+{
+    private:
+        Tensor::Tensor<T> _cachedOutput;
+
+    public:
+        CCELSoftmaxLayer():
+            _cachedOutput({1}) {}
+
+        Tensor::Tensor<T> forward(const Tensor::Tensor<T>& input) override
+        {
+            if (_cachedOutput.shape() != input.shape()) [[unlikely]]
+            {
+                _cachedOutput = Tensor::Tensor<T>(input.shape()); 
+            }
+
+            uint64_t batchSize = input.shape()[0];
+            uint64_t numClasses = input.shape()[1];
+
+            for (uint64_t i = 0; i < batchSize; ++i) 
+            {
+                auto rowStartIn = input.data() + (i * numClasses);
+                auto rowEndIn = rowStartIn + numClasses;
+            
+                auto rowStartOut = _cachedOutput.data() + (i * numClasses);
+                auto rowEndOut = rowStartOut + numClasses;
+            
+                auto maxIter = std::max_element(rowStartIn, rowEndIn);
+                T rowMax = *maxIter;
+
+                std::transform(rowStartIn, 
+                                rowEndIn, 
+                                rowStartOut, 
+                                [rowMax](T val){
+                                    return std::exp(val - rowMax);
+                                });
+                
+                auto localSum = std::accumulate(rowStartOut, rowEndOut, T{0});
+
+                std::transform(rowStartOut,
+                               rowEndOut,
+                               rowStartOut,
+                               [localSum](T val){
+                                   return val / localSum;
+                               });                
+            }
+
+            return _cachedOutput;
+        }
+
+        Tensor::Tensor<T> backward(const Tensor::Tensor<T>& outputGradient, T learningRate) override 
+        {
+           return outputGradient;
         }
 };
