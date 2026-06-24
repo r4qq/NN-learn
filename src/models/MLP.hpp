8941e386f5ec7a7a4b8be46312a2c8ecfd0797ff
r4qq <r4qq@protonmail.com>
2026-06-25 00:17:11 +0200

    model now saves data about its type and amount of layers

--- src/models/MLP.hpp
+++ src/models/MLP.hpp
@@ -1,6 +1,11 @@
+#pragma once
+
 #include "core/config.hpp"
 #include "core/NeuralNetwork.hpp"
 #include "core/Layer.hpp"
+#include "ModelType.hpp"
+#include <cstddef>
+#include <cstdint>
 #include <memory>
 #include <ranges>
 
@@ -8,7 +13,7 @@
 namespace NN::Models 
 {
     template<typename T>
-    class MLP : NN::Core::NeuralNetwork<T>
+    class MLP : public NN::Core::NeuralNetwork<T>
     {
     private:
         std::vector<std::unique_ptr<NN::Core::Layer<T>>> _layers;
@@ -39,10 +44,17 @@ namespace NN::Models
             }
         }
 
-        void save(std::string fileName) override
+        void save(std::string fileName) const override 
         {
             std::ofstream outFile;
             outFile.open(fileName, std::ios::binary);
+            
+            ModelType type = ModelType::MLP;
+            outFile.write(reinterpret_cast<char*>(&type), sizeof(uint32_t));
+
+            size_t layersAmount = _layers.size();
+            outFile.write(reinterpret_cast<char*>(&layersAmount), sizeof(size_t));
+
             for (const auto& layer : _layers) 
             {
                 layer->save(outFile);
